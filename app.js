be4ea57b45390c0dbee36d9f5b0307c7dacd1a86
wallace-y <cameronwfinney@gmail.com>
2023-02-08 17:04:28 +0000

    adding h2, starting content

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res) {
 
+  res.render("home", {
+    startingContent: homeStartingContent
+  })
+
+
+})
 
 
 
